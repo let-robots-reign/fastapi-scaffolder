3add903b32354e46e91eb535bba0097f6d5c446d
alexey.zotov <alexey.zotov@vk.team>
2023-04-09 22:10:34 +0300

    improving api generation

--- fastapi_scaffolder/visitors/controllers.py
+++ fastapi_scaffolder/visitors/controllers.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from fastapi_scaffolder.parser import OpenAPIParser
+from fastapi_scaffolder.parser import OpenAPIParser, Operation
 from fastapi_scaffolder.visitor import Visitor
 
 import inflect
@@ -11,9 +11,30 @@ class ControllersVisitor:
         self.inflect_engine = inflect_engine
 
     def get_singular_of_word(self, word):
-        print(word, end=" ")
-        print(self.inflect_engine.singular_noun(word))
         return self.inflect_engine.singular_noun(word)
+
+    def get_controller_name(self, operation: Operation):
+        return operation.operationId
+
+    def get_method_name(self, operation: Operation):
+        name = operation.function_name
+        if "list" in name:
+            return "get_list"
+        elif "get" in name:
+            return "get_one"
+        elif "create" in name or "new" in name:
+            return "create"
+        elif "edit" in name or "update" in name:
+            return "edit"
+        elif "delete" in name:
+            return "delete_one"
+        return "dummy"
+
+    def get_controller_arguments(self, operation: Operation):
+        return ", ".join(
+            [*(arg[:-1] for arg in operation.snake_case_arguments.split() if arg[-1] == ":"),
+             "session"]
+        )
 
     def remove_unnecessary_models(self, models_names: List[str]):
         """
@@ -36,7 +57,12 @@ class ControllersVisitor:
 
     def get_template_vars(self, parser: OpenAPIParser) -> Dict[str, object]:
         entities_names = list(parser.extra_template_data.keys())
-        return {"models": self.remove_unnecessary_models(entities_names)}
+        return {
+            "models": self.remove_unnecessary_models(entities_names),
+            "get_controller_name": self.get_controller_name,
+            "get_controller_arguments": self.get_controller_arguments,
+            "get_method_name": self.get_method_name
+        }
 
 
 def get_controllers_vars(parser: OpenAPIParser, *args) -> Dict[str, object]:
