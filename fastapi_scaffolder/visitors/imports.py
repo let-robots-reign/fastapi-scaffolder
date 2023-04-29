46ec3d0a044d788b7b991d22c31140af9525a438
alexey.zotov <alexey.zotov@vk.team>
2023-04-29 12:13:07 +0400

    convert all visitors to classes

--- fastapi_scaffolder/visitors/imports.py
+++ fastapi_scaffolder/visitors/imports.py
@@ -9,30 +9,36 @@ from fastapi_scaffolder.parser import OpenAPIParser
 from fastapi_scaffolder.visitor import Visitor
 
 
-def _get_most_of_reference(data_type: DataType) -> Optional[Reference]:
-    if data_type.reference:
-        return data_type.reference
-    for data_type in data_type.data_types:
-        reference = _get_most_of_reference(data_type)
-        if reference:
-            return reference
-    return None
+class ImportsVisitor:
+    @classmethod
+    def _get_most_of_reference(cls, data_type: DataType) -> Optional[Reference]:
+        if data_type.reference:
+            return data_type.reference
+        for data_type in data_type.data_types:
+            reference = cls._get_most_of_reference(data_type)
+            if reference:
+                return reference
+        return None
+
+    def get_imports(self, parser: OpenAPIParser, model_path: Path) -> Dict[str, object]:
+        imports = Imports()
+
+        imports.update(parser.imports)
+        for data_type in parser.data_types:
+            reference = self._get_most_of_reference(data_type)
+            if reference:
+                imports.append(data_type.all_imports)
+                imports.append(
+                    Import.from_full_path(f'.{model_path.stem}.{reference.name}')
+                )
+        for from_, imports_ in parser.imports_for_fastapi.items():
+            imports[from_].update(imports_)
+        return {'imports': imports}
 
 
 def get_imports(parser: OpenAPIParser, model_path: Path) -> Dict[str, object]:
-    imports = Imports()
-
-    imports.update(parser.imports)
-    for data_type in parser.data_types:
-        reference = _get_most_of_reference(data_type)
-        if reference:
-            imports.append(data_type.all_imports)
-            imports.append(
-                Import.from_full_path(f'.{model_path.stem}.{reference.name}')
-            )
-    for from_, imports_ in parser.imports_for_fastapi.items():
-        imports[from_].update(imports_)
-    return {'imports': imports}
+    imports_controller = ImportsVisitor()
+    return imports_controller.get_imports(parser, model_path)
 
 
 visit: Visitor = get_imports
--- fastapi_scaffolder/visitors/operations.py
+++ fastapi_scaffolder/visitors/operations.py
@@ -5,11 +5,17 @@ from fastapi_scaffolder.parser import OpenAPIParser, Operation
 from fastapi_scaffolder.visitor import Visitor
 
 
-def get_operations(parser: OpenAPIParser, model_path: Path) -> Dict[str, object]:
-    sorted_operations: List[Operation] = sorted(
-        parser.operations.values(), key=lambda m: m.path
-    )
-    return {'operations': sorted_operations}
+class OperationsVisitor:
+    def get_operations(self, parser: OpenAPIParser) -> Dict[str, object]:
+        sorted_operations: List[Operation] = sorted(
+            parser.operations.values(), key=lambda m: m.path
+        )
+        return {'operations': sorted_operations}
+
+
+def get_operations(parser: OpenAPIParser, *args) -> Dict[str, object]:
+    operations_visitor = OperationsVisitor()
+    return operations_visitor.get_operations(parser)
 
 
 visit: Visitor = get_operations
